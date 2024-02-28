69e47fe965744826005334170025effcdf98f480
usman-rizwan <usmanrizwan771@gmail.com>
2024-02-28 10:39:29 +0500

    Class Code 28-2-2024

---
+++ Javascript/28-2-2024/app.js
@@ -0,0 +1,69 @@
+// alert("Hello worrld");
+// var name = "usman";
+// var age = 18 ;
+// var fullName  = name;
+// console.log(name)
+// console.log(age)
+// console.log(fullName)
+
+
+// var fullName ; 
+// fullName = "Taha"
+// console.log(fullName)
+
+// var num =20;
+// var num2 = num + 20;
+// console.log(num2)
+
+
+// var num = "20";
+// var num2 = 30;
+// console.log(num - num2)
+           // "20" - 30
+
+
+// var name = "usman";
+// var num = 20;
+// var num2= " "
+// console.log(num + num2 )
+// console.log(num + num2 * name)
+
+// var myName;
+// var my_name ;
+// var name1 ;
+
+// var delete = "usman"
+// console.log(name)
+
+// Post Increment
+var num = 2;
+var newNum = num++
+
+// jahan ham isko call krenge likhenge to purani value assign krega phr plus krega
+console.log('num->',num)
+console.log('newNum->',newNum)
+
+
+// Pre Imcrement
+var pre = 2;
+var preNum = ++pre
+console.log('prenum->',pre)
+console.log('preNum->',preNum)
+
+
+// Post Decrement
+var pd_num = 2;
+var pd_newNum = pd_num--
+
+// jahan ham isko call krenge likhenge to purani value assign krega phr minus krega
+console.log('Post Num->', pd_num)
+console.log('Posy newNum->',pd_newNum)
+
+
+
+
+// Pre Decrement
+var pre = 2;
+var preNum = --pre
+console.log('prenum->',pre)
+console.log('preNum->',preNum)
