e52f95623ceb0f884f841c8867f22491cc546977
Muhammad Usman <usmanrizwan771@gmail.com>
2024-03-13 08:18:09 +0500

    Class code 13-03-2024

---
+++ Javascript/13-3-2024/app.js
@@ -0,0 +1,21 @@
+var cities = ['Karachi', 'Lahore', 'Hyderabad', 'Peshawar']
+
+//slice use hota he copy krne ke lye
+// var copied = cities.slice(1)
+// console.log(copied)
+
+
+// convert string into array , split
+
+var str = 'Hello World! Ramadan is here'
+var splited =  str.split(" ")
+console.log(splited)
+
+
+// convert array into string , join
+
+var arr = ['Hello' , 'Bilal' , 'Do' , 'somthing ']
+
+var joinArr = arr.join(' ')
+
+console.log(joinArr)
