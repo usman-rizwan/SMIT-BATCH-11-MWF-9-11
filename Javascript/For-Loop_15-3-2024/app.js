16ca41620c63d30feea7ebb8f53b6b277152e366
Muhammad Usman <usmanrizwan771@gmail.com>
2024-03-18 08:08:44 +0500

    Class code 15-03-2024

---
+++ Javascript/For-Loop_15-3-2024/app.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+// document.write(cities[0])
+// document.write(cities[1])
+// document.write(cities[2])
+// document.write(cities[3])
+
+
+//For loop
+//while loop
+//do loop
+
+//1
+//12
+//123
+//1234
+//12345
+//123456
+//1234567
+//12345678
+//123456789
+
+//*
+//**
+//***
+//****
+//*****
+//******
+
+
+
+// var tableNumber = prompt('Add Table Number')
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(tableNumber * i)
+// }
+
+var cities = ['Karachi', 'Lahore', 'Islamabad', 'Peshawar']
+
+for (var i = 0; i < cities.length; i++) {
+    console.log(cities[i] )
+}
