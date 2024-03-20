3f53e18089dbde560812d10c3bd9ba03d58a39f5
usman-rizwan <usmanrizwan771@gmail.com>
2024-03-20 08:30:04 +0500

    class code 20-03-2024

--- Javascript/18-3-2024/app.js
+++ Javascript/18-3-2024/app.js
@@ -3,18 +3,66 @@
 // }
 
 
-var cities = ["karachi", "lahore", "islamabad", "Hyderabad", "quetta"];
-var userInput = prompt("Enter city name");
-var isFound = false;
-for (var i = 0; i < cities.length; i++) {
-    // console.log(cities[i])
-    if (userInput === cities[i]) {
-        console.log("city found " + userInput)
-        isFound = true
-        break;
-    }
-}
-if (isFound === false) {
-    console.log("Not Found")
-}
+// var cities = ["karachi", "lahore", "islamabad", "Hyderabad", "quetta"];
+// var userInput = prompt("Enter city name");
+// var isFound = false;
+// for (var i = 0; i < cities.length; i++) {
+//     // console.log(cities[i])
+//     if (userInput === cities[i]) {
+//         console.log("city found " + userInput)
+//         isFound = true
+//         break;
+//     }
+// }
+// if (isFound === false) {
+//     console.log("Not Found")
+// }
 // console.log(cities[i])
+
+
+//print even number
+
+// for (var i = 0; i <= 20; i++) {
+//     if (i % 2 == 0) {
+//         console.log('Even Number ', i)
+//     }
+// }
+
+//print odd number
+// for (var i = 0; i <= 20; i++) {
+//     if (i % 2 !== 0) {
+//         console.log('Odd Number ', i)
+//     }
+// }
+
+//print weekdays
+// var weekdays = ['Mon', 'Tue', 'Wed', 'Thurs']
+// for (var i = 0; i < weekdays.length; i++) {
+//     console.log(weekdays[i])
+// }
+
+
+// for (var i = 0; i < 10; i++) {
+//     console.log('Hello World')
+// }
+
+// print square of number from 1 to 10
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i * i)
+// }
+
+// find largest number in an array
+
+// var numbers = [10, 5, 30, 53, 32, 499, 354]
+
+// var largestNumber = 0
+// for (var i = 0; i < numbers.length; i++) {
+//     if (numbers[i] > largestNumber) {
+//         largestNumber = numbers[i]
+//     }
+//     console.log(largestNumber)
+// }
+
+
+
+// smallest number 
