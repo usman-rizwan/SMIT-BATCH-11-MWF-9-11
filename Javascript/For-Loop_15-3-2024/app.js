38043bfdac82d01783dbba6e2b6c06246bda8c34
usman-rizwan <usmanrizwan771@gmail.com>
2024-03-25 08:29:34 +0500

    class code 25-03-2024

--- Javascript/For-Loop_15-3-2024/app.js
+++ Javascript/For-Loop_15-3-2024/app.js
@@ -38,8 +38,64 @@
 //     console.log(tableNumber * i)
 // }
 
-var cities = ['Karachi', 'Lahore', 'Islamabad', 'Peshawar']
+// var cities = ['Karachi', 'Lahore', 'Islamabad', 'Peshawar']
 
-for (var i = 0; i < cities.length; i++) {
-    console.log(cities[i] )
+// for (var i = 0; i < cities.length; i++) {
+//     console.log(cities[i])
+// }
+
+//1,3,6,10,15,21,28,36,45
+
+// var num = 0
+// for (var i = 1; i < 10; i++) {
+//     num = num + i
+//     console.log(num)
+// }
+
+
+// for (var i = 1; i < 10; i++) {
+//     console.log(i, 10 - i)
+// }
+
+
+//hello
+
+// var strArr = 'hello'.split('')
+// var strArrRev = strArr.reverse()
+// var strArrJoin = strArrRev.join('')
+// console.log(strArrJoin)
+
+// var nam = prompt('Add name to check is it Pallindrome')
+// var isItPallindrome = true
+
+// for (var i = 0; i < nam.length; i++) {
+//     var start = nam[i]
+//     var end = nam[(nam.length - 1) - i]
+//     console.log(start)
+//     console.log(end)
+//     if (start != end) {
+//         isItPallindrome = false
+//     }
+// }
+// if (isItPallindrome) {
+//     alert('It is pallindrome')
+// } else {
+//     alert('It is not pallindrome')
+// }
+
+var buffe = ['karahi', 'biryani', 'samosa', 'ande', 'aaloo', 'chole',
+    'gol gappay']
+
+var userItem = prompt('Item')
+
+var isFound = false
+for (var i = 0; i < buffe.length; i++) {
+    if (buffe[i] === userItem) {
+        isFound = true
+    }
+}
+if (isFound) {
+    document.write('It is available')
+} else {
+    document.write('It is not available')
 }
