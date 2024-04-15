a705a9b5d713509d908c514e76707761340a1f15
Muhammad Usman <usmanrizwan771@gmail.com>
2024-04-15 11:12:32 +0500

    Class code 15-04-2024

---
+++ Javascript/Nested_loop_27-3-2024/app.js
@@ -0,0 +1,47 @@
+
+
+
+// var prizeBonds = [123, 321, 232, 233, 211, 242, 200]
+// var prizeBondsOpened = [113, 121, 432, 133, 231, 242, 900,
+//     700, 656, 432, 212, 444, 555, 66, 233, 742, 433]
+
+// var userWon = []
+
+// for (var i = 0; i < prizeBonds.length; i++) {
+//     for (var j = 0; j < prizeBondsOpened.length; j++) {
+//         console.log("i=>", prizeBonds[i], "j=>", prizeBondsOpened[j])
+//         if (prizeBonds[i] === prizeBondsOpened[j]) {
+//             console.log("Number", prizeBonds[i], i, j)
+//             userWon.push(prizeBonds[i])
+//         }
+//     }
+// }
+// console.log("User Won=>", userWon)
+
+
+
+
+
+for (var i = 0; i < 2; i++) {
+    for (var j = 0; j < 3; j++) {
+        console.log("j=>", i + j)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Javascript/String Methods/app.js
@@ -0,0 +1,45 @@
+
+
+var a = 'a'
+var A = 'A'
+
+
+// console.log(a === A.toLowerCase())
+// console.log(a.toUpperCase() === A)
+
+//lowercase uppercase example
+
+// var cities = ['kaRachi', 'lahore', 'islamabad']
+
+// var userCity = prompt('Enter your city')
+
+// for (var i = 0; i < cities.length; i++) {
+//     if (userCity.toLowerCase() == cities[i].toLowerCase()) {
+//         alert("It's in our list")
+//         break
+//     }
+// }
+
+
+
+// string length and copy string
+
+// var smit = 'saylani'
+
+// console.log(smit.slice(1))
+
+// console.log(smit[0].toUpperCase() + smit.slice(1))
+
+// console.log(smit[6])
+
+
+
+
+var txt = 'Which platform you will be using for the payment.'
+
+// if(abc == ' ')
+// for (var i = 0; i < txt.length; i++) {
+//     if (txt.slice(i, i + 7) === 'payment') {
+//         alert('Payment word is not allowed')
+//     }
+// }
