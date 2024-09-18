1b6b02548513dd25c3d8494458689ffaffb35e13
Bilal raza <attari1235@gmail.com>
2024-09-18 19:06:55 +0500

    logout button on POS

--- React/pos/src/pages/POS/POS.jsx
+++ React/pos/src/pages/POS/POS.jsx
@@ -1,5 +1,14 @@
+import { Button } from "antd";
+import { signOut } from "firebase/auth";
+import { auth } from "../../utils/firebase";
+
 function POS() {
-  return <h1>POS</h1>;
+  return <div>
+    <div className="flex justify-between">
+    <h1>POS</h1>
+    <Button onClick={async()=> await signOut(auth)}>Logout</Button>
+    </div>
+  </div>;
 }
 
 export default POS;
